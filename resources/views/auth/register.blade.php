08f592d2929c36e6ede6575da33d45a7bfda0e62
GABY PUERTO <88343868+gabypuertor964@users.noreply.github.com>
2023-09-06 02:39:33 -0500

    Avance y Sobre escritura Forms

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,74 +1,89 @@
-{{-- Declaracion e importacion componente principal --}}
-@extends('layouts.auth')
+@extends('adminlte::auth.register')
 
-{{-- Declaracion complemtento etiqueta litle del Header --}}
-@section('title','Login')
+@section('auth_body')
+    <form action="" method="post">
+        @csrf
 
-{{-- Declaracion contenido principal de la pagina web --}}
-@section('content')
+        PEPE
 
-  <form action="{{route("clientRegister")}}" method="post">
-    {{--Tokem de Seguridad--}}
-    @csrf
+        {{-- Name field --}}
+        <div class="input-group mb-3">
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
+                   value="{{ old('name') }}" placeholder="{{ __('adminlte::adminlte.full_name') }}" autofocus>
 
-    <div class="mb-3">
-      <label for="nombres" class="form-label">Nombres:</label>
-      <input type="text" class="form-control" name="nombres" id="nombres" required max='30'>
-    </div>
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-user {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                </div>
+            </div>
 
-    <div class="mb-3">
-      <label for="apellidos" class="form-label">Apellidos:</label>
-      <input type="text" class="form-control" name="apellidos" id="apellidos" required max='30'>
-    </div>
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
 
-    <div class="mb-3">
-      <label for="id_sexo" class="form-label">Sexo</label>
-      <select class="form-select" name="id_sexo" id="id_sexo" required>
-        <option selected >Seleccione</option>  
-        @foreach ($sexos as $sexo)
-          <option value="{{$sexo->id}}">{{$sexo->nombre}}</option>
-        @endforeach
-      </select>
-    </div>
+        {{-- Email field --}}
+        <div class="input-group mb-3">
+            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+                   value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}">
 
-    <div class="mb-3">
-      <label for="id_tip_doc" class="form-label">Tipo Documento</label>
-      <select class="form-select" name="id_tip_doc" id="id_tip_doc" required>
-        <option selected >Seleccione</option>  
-        @foreach ($tipos_documento as $tipo_documento)
-          <option value="{{$tipo_documento->id}}">{{$tipo_documento->siglas}} - {{$tipo_documento->nombre}}</option>
-        @endforeach
-      </select>
-    </div>
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-envelope {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                </div>
+            </div>
 
-    <div class="mb-3">
-      <label for="num_doc" class="form-label">Numero de Documento:</label>
-      <input type="number" class="form-control" name="num_doc" id="num_doc" required>
-    </div>
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
 
-    <div class="mb-3">
-      <label for="num_tel" class="form-label">Numero Telefonico:</label>
-      <input type="number" class="form-control" name="num_tel" id="num_tel" required>
-    </div>
+        {{-- Password field --}}
+        <div class="input-group mb-3">
+            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                   placeholder="{{ __('adminlte::adminlte.password') }}">
 
-    <div class="mb-3">
-      <label for="fecha_nacimiento" class="form-label">Fecha de nacimiento:</label>
-      <input type="date" class="form-control" name="fecha_nacimiento" id="fecha_nacimiento" required>
-    </div>
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                </div>
+            </div>
 
-    <div class="mb-3">
-      <label for="email" class="form-label">Correo Electronico:</label>
-      <input type="email" class="form-control" name="email" id="email" required>
-    </div>
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
 
-    <div class="mb-3">
-      <label for="password" class="form-label">Contraseña:</label>
-      <input type="password" class="form-control" name="password" id="password" required>
-    </div>
+        {{-- Confirm password field --}}
+        <div class="input-group mb-3">
+            <input type="password" name="password_confirmation"
+                   class="form-control @error('password_confirmation') is-invalid @enderror"
+                   placeholder="{{ __('adminlte::adminlte.retype_password') }}">
 
-    <button type="submit" class="btn btn-primary">Registrarse</button>
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                </div>
+            </div>
 
-    <a href="{{route('login')}}">¿Ya tienes cuenta?, Inicia Sesion</a>
-  </form>
+            @error('password_confirmation')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
 
-@endsection
+        {{-- Register button --}}
+        <button type="submit" class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
+            <span class="fas fa-user-plus"></span>
+            {{ __('adminlte::adminlte.register') }}
+        </button>
+
+    </form>
+@stop
