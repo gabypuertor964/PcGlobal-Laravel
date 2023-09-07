a84c7eb7f16401f2e5efa0a027dc1c39e6629bf3
GABY PUERTO <88343868+gabypuertor964@users.noreply.github.com>
2023-09-06 19:12:45 -0500

    Clean Templates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,16 @@
-@extends('adminlte::auth.login')
+{{-- Declaracion e importacion componente principal --}}
+@extends('layouts.auth')
+
+{{-- Declaracion complemtento etiqueta litle del Header --}}
+@section('title','Login')
+
+{{-- Declaracion contenido principal de la pagina web --}}
+@section('content')
+
+  {{-- Declaracion y envio de clases personalidas a la etiqueta body presente en el componente principal--}}
+  @section('body_class','')
+
+  {{-- Envio de clases personalizadas a la etiqueta main, la cual se encuentra en el componente principal--}}
+  @section('main_class','')
+
+@endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -35,24 +35,14 @@
         Explicacion: Esta directiva declara un componente asignándole un nombre y un valor por defecto en caso de no declararse en la vista que importa el componente.
         
         Nota: En caso de no declarar un valor por defecto y tampoco asignarle un valor en la vista que importa el componente, laravel reportará error
-        --}}
-        <body class="@yield('body_class','')">
-            
-            {{-- Importancion Barra de navegacion --}}
-            @include('components.navbars.landing_page')
-            
-            <main class="@yield('main_class','')">
+    --}}
+    <body class="@yield('body_class','')">
+        <main class="@yield('main_class','')">
                 
-                {{-- Importacion contenido principal pagina web --}}
-                @yield('content')
+            {{-- Importacion contenido principal pagina web --}}
+            @yield('content')
                 
-            </main>
-            
-            {{-- Importancion Pie de Pagina --}}
-            @include('components.footer')
-        </body>
-        {{-- Importación manual de archivos JavaScript --}}
-        <script src="{{ asset('js/app.js') }}"></script>
-        
-        </html>
+        </main>
+    </body>
+</html>
         
