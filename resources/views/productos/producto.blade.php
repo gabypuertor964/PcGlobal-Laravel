ed65ae83497fa114632980b8bda9c1b10db40188
GABY PUERTO <88343868+gabypuertor964@users.noreply.github.com>
2023-09-06 20:46:02 -0500

    Reinstalation

--- resources/views/productos/producto.blade.php
+++ resources/views/productos/producto.blade.php
@@ -14,7 +14,7 @@
 
   <div class="contenedor mx-8 my-3 text-sm hover:underline">
     <a href="{{route('index')}}" class="hover:text-blue-600 hover:underline inline-flex items-center gap-1">
-        <i class="fa-solid fa-share fa-flip-horizontal"></i> <p>Volver</p>
+        <i class="fa-solid fa-share fa-flip-horizontal"></i><p>Volver</p>
     </a>
   </div>
   <div class="contenedor grid grid-cols-1 md:grid-cols-3 my-6 gap-4 py-3 bg-white px-3 mx-8 rounded-lg"> 
@@ -24,6 +24,7 @@
       <aside class="flex flex-col p-2 sm:p-3 desc-producto gap-y-2 text-start ms-3 sm:ms-0 ">
         <p class="text-gray-400 text-xs">Descripción del producto</p>
         <ul class="flex flex-col gap-y-2 sm:gap-y-4">
+            <li class="text-lg lg:text-lg -mb-2 lg:-mb-4"><p class="font-semibold">{{$marca->nombre}}</p></li>
             <li class="text-xl lg:text-2xl hover:underline"><p class="font-bold">{{$producto->modelo}}</p></li>
             <li class="text-lg lg:text-xl">$ {{number_format($producto->precio, 0, '.', '.')}}</li>
         </ul>
