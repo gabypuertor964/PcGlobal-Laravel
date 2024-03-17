37f499775aa57f427fd961b1a7cc50459ab36a31
DanielEspitia1507 <134316312+DanielEspitia1507@users.noreply.github.com>
2024-03-17 00:38:45 -0500

    Actualización de vite

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,6 @@ export default defineConfig({
                 "resources/js/navbar.js",
                 "resources/js/scroll.js",
                 "resources/js/select_preview.js",
-                "resources/js/fade-alert.js",
             ],
             refresh: true,
         }),
